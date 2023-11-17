c2aab220f41d249d5e72a4fa368af9d09a1562c4
hoalasama <hoalasamaeu@gmail.com>
2023-11-17 14:29:17 +0700

    admin interface

--- project/userincome/admin.py
+++ project/userincome/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import UserIncome, Source
 # Register your models here.
 
-admin.site.register(UserIncome)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('amount', 'description', 'owner', 'source', 'date',)
+
+admin.site.register(UserIncome, IncomeAdmin)
 admin.site.register(Source)
